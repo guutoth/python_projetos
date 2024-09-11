ee407c2a3abdf50ae5b039d30f305fb0199d215e
Augusto Adams Thomas <gutoadthomas@gmail.com>
2024-09-11 11:30:25 -0300

    Update Instalarpacotes.bat

--- Instalarpacotes.bat
+++ Instalarpacotes.bat
@@ -1,4 +1,4 @@
-@echo off
+o@echo off
 setlocal
 
 :: Instala os pacotes necessarios via pip
@@ -9,6 +9,6 @@ pip install requests beautifulsoup4 pandas winshell pywin32 pyinstaller || (
     exit /b 1
 )
 
-echo Todos os pacotes foram instalados com sucesso.
+echo Todos os pacotes foram instalados com sucesso. Pressione ENTER para continuar
 
 pause
