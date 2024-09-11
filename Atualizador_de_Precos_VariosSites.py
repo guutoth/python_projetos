52f0633b599103cc792c134ddcff6ff88fdc3759
Augusto Adams Thomas <gutoadthomas@gmail.com>
2024-09-11 16:18:15 -0300

    Update Atualizador_de_Precos_VariosSites.py

--- Atualizador_de_Precos_VariosSites.py
+++ Atualizador_de_Precos_VariosSites.py
@@ -1,11 +1,10 @@
-### VERSÃO 2.4.1 ###
+### VERSÃO 2.4.4 ###
 # UPDATES:
 # 1 - REFORMULAÇÃO DAS BARRAS E BOTÕES
 # 2 - OPÇÃO DE ADICIONAR PRODUTOS DO MERCADO LIVRE E LOJAS QUERO QUERO
 # 3 - ADICIONADA UMA COLUNA QUE MOSTRA O SITE 
 # 4 - MELHORIAS NA INTERFACE
 # 5 - MELHORIAS NO DESEMPENHO
-
 import requests
 from bs4 import BeautifulSoup
 import tkinter as tk
@@ -16,16 +15,21 @@ import pandas as pd
 import concurrent.futures
 import shutil
 from datetime import datetime
-import time
 import threading
+import logging
+
+# Configuração do logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Constantes de configuração
-NOME_PROGRAMA = "Atualizador de Preços (ML e Quero-Quero) - Versão 2.4.1"
+NOME_PROGRAMA = "Atualizador de Preços (ML e Quero-Quero) - Versão 2.4.4"
 CAMINHO_ARQUIVO = f'C:\\Atualizador de Preços (ML e Quero-Quero)\\produtos.txt'
 DIRETORIO_BACKUP = f'C:\\Atualizador de Preços (ML e Quero-Quero)\\backup\\'
 COLUNAS = ('Produto', 'Preço', 'Link', 'Site')
 
-# Funções específicas para diferentes sites
+# Variáveis globais
+root = None  # Inicializa como None e será definido na função criar_interface
 
 
 def obter_nome_e_preco_queroquero(url):
@@ -34,7 +38,7 @@ def obter_nome_e_preco_queroquero(url):
         response = requests.get(url, headers=headers, timeout=20)
         response.raise_for_status()
     except requests.RequestException as e:
-        print(f"Erro ao acessar {url}: {e}")
+        logging.error(f"Erro ao acessar {url}: {e}")
         return 'Nome não encontrado', 'Preço não encontrado', url, 'queroquero.com'
 
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -57,7 +61,7 @@ def obter_nome_e_preco_mercadolivre(url):
         response = requests.get(url, headers=headers, timeout=20)
         response.raise_for_status()
     except requests.RequestException as e:
-        print(f"Erro ao acessar {url}: {e}")
+        logging.error(f"Erro ao acessar {url}: {e}")
         return 'Nome não encontrado', 'Preço não encontrado', url, 'mercadolivre.com'
 
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -84,6 +88,7 @@ def obter_nome_e_preco(url):
 def ler_links_arquivo(arquivo):
     caminho_arquivo = os.path.join(os.path.dirname(__file__), arquivo)
     if not os.path.isfile(caminho_arquivo):
+        logging.warning(f"O arquivo {caminho_arquivo} não existe.")
         return []
     with open(caminho_arquivo, 'r') as f:
         return f.read().splitlines()
@@ -92,16 +97,16 @@ def ler_links_arquivo(arquivo):
 def fazer_backup(caminho_arquivo):
     if not os.path.exists(DIRETORIO_BACKUP):
         os.makedirs(DIRETORIO_BACKUP, exist_ok=True)
-        print(f"Pasta de backup criada em: {DIRETORIO_BACKUP}")
+        logging.info(f"Pasta de backup criada em: {DIRETORIO_BACKUP}")
 
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     backup_arquivo = os.path.join(
         DIRETORIO_BACKUP, f'produtos_backup_{timestamp}.txt')
     try:
         shutil.copy(caminho_arquivo, backup_arquivo)
-        print(f"Backup criado em: {backup_arquivo}")
+        logging.info(f"Backup criado em: {backup_arquivo}")
     except Exception as e:
-        print(f"Erro ao criar o backup: {e}")
+        logging.error(f"Erro ao criar o backup: {e}")
 
 
 def adicionar_link():
@@ -159,7 +164,6 @@ def abrir_link(event):
 
 def atualizar_lista(ordenar_por=None):
     def processar_lista():
-        global progress_bar, status_label
         urls_produtos = ler_links_arquivo(CAMINHO_ARQUIVO)
         tree.delete(*tree.get_children())
         progress_bar['value'] = 0
@@ -180,15 +184,15 @@ def atualizar_lista(ordenar_por=None):
             nonlocal produtos
             urls_já_exibidas = set()
             with concurrent.futures.ThreadPoolExecutor() as executor:
-                futuros = [executor.submit(obter_nome_e_preco, url)
-                           for url in urls]
+                futuros = [executor.submit(
+                    obter_nome_e_preco, url) for url in urls]
                 for futuro in concurrent.futures.as_completed(futuros):
                     nome, preco, url, site = futuro.result()
                     if url not in urls_já_exibidas:
                         produtos.append((nome, preco, url, site))
                         urls_já_exibidas.add(url)
-                    progress_bar['value'] += 1
-                    root.update_idletasks()
+                    root.after(0, lambda: progress_bar.configure(
+                        value=progress_bar['value'] + 1))
 
         processar_urls(urls_produtos)
         produtos = ordenar_produtos(produtos, ordenar_por)
@@ -202,7 +206,6 @@ def atualizar_lista(ordenar_por=None):
         status_label.config(text="Atualização concluída.")
         root.after(5000, limpar_status)
 
-    # Executar em uma nova thread
     threading.Thread(target=processar_lista).start()
 
 
@@ -226,114 +229,81 @@ def ajustar_largura_colunas():
             largura_max = max(int(largura_max * 0.20), 20)
         else:
             tree.column(coluna, width=largura_max * 10)
-        tree.column(coluna, width=largura_max,
-                    anchor=tk.CENTER)  # Centralizado
+        # Ajusta a largura de cada coluna
+        tree.column(coluna, width=largura_max, anchor=tk.CENTER)
 
 
 def exportar_para_excel():
     try:
-        dados = [tree.item(item, "values") for item in tree.get_children()]
-        if not dados:
-            messagebox.showwarning("Exportação falhou",
-                                   "Não há dados para exportar.")
-            return
-
-        df = pd.DataFrame(dados, columns=["Produto", "Preço", "Link", "Site"])
         caminho_excel = filedialog.asksaveasfilename(
-            defaultextension=".xlsx",
-            filetypes=[("Excel files", "*.xlsx"), ("All files", "*.*")],
-            title="Salvar como"
-        )
+            defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
         if caminho_excel:
+            dados = [(tree.item(item, "values"))
+                     for item in tree.get_children()]
+            df = pd.DataFrame(dados, columns=COLUNAS)
             df.to_excel(caminho_excel, index=False)
-            messagebox.showinfo("Exportação concluída",
-                                f"Dados exportados para {caminho_excel}")
+            logging.info(f"Dados exportados para {caminho_excel}")
     except Exception as e:
-        print(f"Erro ao exportar para Excel: {e}")
-
-
-def excluir_item():
-    try:
-        selecionado = tree.selection()
-        if not selecionado:
-            messagebox.showwarning(
-                "Nenhum item selecionado", "Selecione um item para excluir.")
-            return
-        item = selecionado[0]
-        link = tree.item(item, "values")[2]
-        excluir_do_arquivo(link)
-        atualizar_lista()
-    except Exception as e:
-        print(f"Erro ao excluir item: {e}")
+        logging.error(f"Erro ao exportar para Excel: {e}")
+        messagebox.showerror("Erro", f"Erro ao exportar para Excel: {e}")
 
 
 def criar_interface():
-    global root, caixa_link, tree, status_label, progress_bar
+    global root, caixa_link, tree, progress_bar, status_label
 
     root = tk.Tk()
     root.title(NOME_PROGRAMA)
     root.geometry("1000x600")
-    root.resizable(True, True)
 
-    frame_insercao = tk.Frame(root)
-    frame_insercao.pack(pady=10)
+    # Frame superior
+    frame_superior = tk.Frame(root)
+    frame_superior.pack(pady=10)
 
-    tk.Label(frame_insercao, text="Adicionar Novo Link:").pack(
-        side=tk.LEFT, padx=5)
-    caixa_link = tk.Entry(frame_insercao, width=50)
-    caixa_link.pack(side=tk.LEFT, padx=5)
+    tk.Label(frame_superior, text="Link do produto:").grid(
+        row=0, column=0, padx=10)
+    caixa_link = tk.Entry(frame_superior, width=80)
+    caixa_link.grid(row=0, column=1, padx=10)
+    tk.Button(frame_superior, text="Adicionar Novo Produto",
+              command=adicionar_link).grid(row=0, column=2, padx=10)
 
-    botao_adicionar = tk.Button(
-        frame_insercao, text="Adicionar Novo Produto", command=adicionar_link)
-    botao_adicionar.pack(side=tk.LEFT, padx=5)
+    # Frame inferior
+    frame_inferior = tk.Frame(root)
+    frame_inferior.pack(pady=10)
 
-    frame_botoes = tk.Frame(root)
-    frame_botoes.pack(pady=10)
+    tk.Button(frame_inferior, text="Atualizar Lista",
+              command=lambda: atualizar_lista()).grid(row=0, column=0, padx=10)
+    tk.Button(frame_inferior, text="Exportar para Excel",
+              command=exportar_para_excel).grid(row=0, column=1, padx=10)
+    tk.Button(frame_inferior, text="Excluir Selecionado", command=lambda: excluir_do_arquivo(
+        tree.item(tree.selection()[0], "values")[2])).grid(row=0, column=2, padx=10)
+    tk.Button(frame_inferior, text="Ordenar por Nome", command=lambda: atualizar_lista(
+        ordenar_por='nome')).grid(row=1, column=0, padx=10)
+    tk.Button(frame_inferior, text="Ordenar por Preço", command=lambda: atualizar_lista(
+        ordenar_por='preco')).grid(row=1, column=1, padx=10)
 
-    botao_atualizar = tk.Button(
-        frame_botoes, text="Atualizar Preços", command=lambda: atualizar_lista())
-    botao_atualizar.pack(side=tk.LEFT, padx=5)
+    # Status e Barra de progresso
+    status_label = tk.Label(root, text="Pronto")
+    status_label.pack(pady=5)
+    progress_bar = ttk.Progressbar(
+        root, orient="horizontal", length=800, mode="determinate")
+    progress_bar.pack(pady=10)
 
-    botao_exportar = tk.Button(
-        frame_botoes, text="Exportar para Excel", command=exportar_para_excel)
-    botao_exportar.pack(side=tk.LEFT, padx=5)
-
-    botao_excluir = tk.Button(
-        frame_botoes, text="Excluir Selecionado", command=excluir_item)
-    botao_excluir.pack(side=tk.LEFT, padx=5)
-
-    frame_ordem = tk.Frame(root)
-    frame_ordem.pack(pady=10)
-
-    botao_ordenar_nome = tk.Button(
-        frame_ordem, text="Ordenar por Nome", command=lambda: atualizar_lista(ordenar_por='nome'))
-    botao_ordenar_nome.pack(side=tk.LEFT, padx=5)
-
-    botao_ordenar_preco = tk.Button(
-        frame_ordem, text="Ordenar por Preço", command=lambda: atualizar_lista(ordenar_por='preco'))
-    botao_ordenar_preco.pack(side=tk.LEFT, padx=5)
-
+    # TreeView
     tree = ttk.Treeview(root, columns=COLUNAS, show='headings')
-    tree.pack(expand=True, fill='both', padx=10, pady=10)
-    tree.bind('<Double-1>', abrir_link)
-
     for coluna in COLUNAS:
         tree.heading(coluna, text=coluna)
-        tree.column(coluna, anchor=tk.CENTER)  # Centralizado
-
-    progress_bar = ttk.Progressbar(
-        root, orient=tk.HORIZONTAL, length=1000, mode='determinate')
-    progress_bar.pack(fill='x', padx=10, pady=10)
-
-    status_label = tk.Label(root, text="Pronto")
-    status_label.pack(pady=10)
+        tree.column(coluna, width=50, anchor=tk.CENTER)
+    tree.bind('<Double-1>', abrir_link)
+    tree.pack(expand=True, fill='both', padx=10)
 
     atualizar_lista()
 
     root.mainloop()
 
 
-criar_interface()
+if __name__ == "__main__":
+    criar_interface()
+
 
 
 
