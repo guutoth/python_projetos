a9a5a6bd62eaf7e7fd3c995fd5a50b9abbf3d68d
Augusto Adams Thomas <gutoadthomas@gmail.com>
2024-09-11 11:09:22 -0300

    Add files via upload

---
+++ 1 - desinstalar pacotes.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:: Desinstala os pacotes necessarios via pip
+echo Iniciando a desinstalacao dos pacotes...
+pip uninstall -y requests beautifulsoup4 pandas winshell pywin32 pyinstaller || (
+    echo Ocorreu um erro ao desinstalar os pacotes.
+    pause
+    exit /b 1
+)
+
+echo Todos os pacotes foram desinstalados com sucesso.
+
+pause
---
+++ 2 - Instalar pacotes.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+:: Instala os pacotes necessarios via pip
+echo Iniciando a instalacao dos pacotes necessarios...
+pip install requests beautifulsoup4 pandas winshell pywin32 pyinstaller || (
+    echo Ocorreu um erro ao instalar os pacotes.
+    pause
+    exit /b 1
+)
+
+echo Todos os pacotes foram instalados com sucesso.
+
+pause
